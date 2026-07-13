b4094263e70716b13b3fd1e619f44b72bfef1c65
TheGhostOfYankeeJim <89273951+TheGhostOfYankeeJim@users.noreply.github.com>
2026-07-13 16:10:01 -0400

    populated my test range
    initial setup funcs

--- awsSecretSniffer/awsSecretSniffer/awsSecretSniffer.py
+++ awsSecretSniffer/awsSecretSniffer/awsSecretSniffer.py
@@ -15,16 +15,32 @@ s3Client = session.client('s3')
 
 response = s3Client.list_buckets()
 
+bucketCount = 0
+
 print("These are the following buckets I can see:")
+
+# Might split this into a count and then ask if they want to print all bucket names
 for bucket in response['Buckets']:
+    bucketCount = bucketCount + 1
+
+    # poor mans debugger
     print(bucket['Name'])
+
+print("Total Buckets Found: " + str(bucketCount))
+
+# take bucket name func
+# CODE
+
+# list contents of buckets
+
+
 
 
 
 #s3 Glacier (Not sure how viable this will be)
 
 #lambda Func
-lamdaObj = boto3.client('lambda')
+#lamdaObj = boto3.client('lambda')
 
 #EFS Func
 
