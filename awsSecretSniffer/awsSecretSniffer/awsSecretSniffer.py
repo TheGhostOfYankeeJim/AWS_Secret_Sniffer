9f9f989df4b9e209904acbbd172cf4429d2cef4e
TheGhostOfYankeeJim <89273951+TheGhostOfYankeeJim@users.noreply.github.com>
2026-07-13 16:37:47 -0400

    Added Bucket Content Lister

--- awsSecretSniffer/awsSecretSniffer/awsSecretSniffer.py
+++ awsSecretSniffer/awsSecretSniffer/awsSecretSniffer.py
@@ -7,6 +7,11 @@ awsSecret = input("What is your AWS Secret:")
 
 
 #s3 Bucket Fucn
+#Will refactor all this into an actual function
+#Will have three modes
+#Kitchen Sink == Searches ALL Buckets, ALL Files, for all secrets
+#Paranoid, list bucket names and focuses on specific buckets and files to search for AWS secrets
+#Recon collects bucket names, names of files in each bucket, exports it to a text file//html report to review later. But no string searches.
 session = boto3.Session(
     aws_access_key_id=awsUser,
     aws_secret_access_key=awsSecret)
@@ -23,15 +28,41 @@ print("These are the following buckets I can see:")
 for bucket in response['Buckets']:
     bucketCount = bucketCount + 1
 
-    # poor mans debugger
-    print(bucket['Name'])
-
+print()
 print("Total Buckets Found: " + str(bucketCount))
 
-# take bucket name func
-# CODE
+print()
 
-# list contents of buckets
+listBucketsResponse = input("Would you like to list the Buckets now? (y/n)")
+listBucketsResponse.lower()
+
+
+if listBucketsResponse == "y" or "yes":
+    for bucket in response['Buckets']:
+        # poor mans debugger
+        print(bucket['Name'])
+
+#maybe a search bucket for sensitive strings func?
+#CODE
+
+# take bucket name func
+targetBucket = input("Which bucket would you like to target?")
+objectCount = 0
+
+# Create paginator Obj for to hold the list of objects
+paginator = s3Client.get_paginator('list_objects_v2')
+
+# Create iterator for use in loop
+page_iterator = paginator.paginate(Bucket=targetBucket)
+
+# Loop
+for page in page_iterator:
+    if 'Contents' in page:
+        for obj in page['Contents']:
+            objectCount = objectCount + 1
+
+            # poor mans debugger
+            print(obj['Key'])
 
 
 
@@ -47,3 +78,5 @@ print("Total Buckets Found: " + str(bucketCount))
 #FSx Func
 
 #EBS Func
+
+#MetaData Search Func?
